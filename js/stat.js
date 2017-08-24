3c2426782132a1a8981669478205686832d56945
Polina Bobrovnik <polinabobrovnik@gmail.com>
2017-08-24 10:45:17 +0300

    Исправление ошибок

--- js/stat.js
+++ js/stat.js
@@ -15,33 +15,32 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
   var max = -1;
-  var maxIndex = -1;
 
-  for (var i = 0 ; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
-      maxIndex = i;
     }
   }
 
   var histogramHeight = 150;
-  var step = histogramHeight / (max - 0);
+  var step = histogramHeight / (max);
   var barWidth = 40;
   var indent = 80;
   var initialX = 160;
   var initialY = 240;
-  var lineWidth = 15;
 
-  for(var i = 0; i < times.length; i++) {
-    if (names[i] == "Вы") {
+  for (i = 0; i < times.length; i++) {
+    if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else ctx.fillStyle = 'rgba(0, 0, 255, '+(Math.random()+0.1)+')';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + (Math.random() * 0.9 + 0.1) + ')';
+    }
 
     ctx.fillRect(initialX + indent * i, initialY, barWidth, -(times[i] * step));
 
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], initialX + indent * i , initialY + 20);
+    ctx.fillText(names[i], initialX + indent * i, initialY + 20);
     ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY - (times[i] * step) - 10);
   }
 };
